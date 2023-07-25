a40a031b1ce36bf593675a19f066b324840d18fb
OnurNovruzlu <onur.novruzlu@mail.ru>
2023-07-25 11:18:51 +0400

    test

--- src/test/java/com/example/FakeDbAppApplicationTests.java
+++ src/test/java/com/example/FakeDbAppApplicationTests.java
@@ -2,7 +2,7 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+// test
 @SpringBootTest
 class FakeDbAppApplicationTests {
 
